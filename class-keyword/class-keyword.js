6a86354b38bb1e9f1d6b53ecaccfc2500443644b
Misael Calvillo Mancilla <calvillomisa@hotmail.com>
2018-04-09 19:17:26 -0500

    coding exersice - class keyword excercises

---
+++ class-keyword/class-keyword.js
@@ -0,0 +1,27 @@
+
+/* 1 - Create a class for a Person. Each person should have a firstName, lastName, favoriteColor, favoriteNumber.
+    
+  2 - Add an instance method called multiplyFavoriteNumber that accepts one parameter and returns the product of the parameter multiplied with the favoriteNumber property on a person object.
+    
+  Examples:    
+    var person = new Person("Elie", "Schoppik", "purple", 34)
+    person.multiplyFavoriteNumber(10) // 340
+*/
+
+class Person {
+  constructor(firstName, lastName, favoriteColor, favoriteNumber){
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.favoriteColor = favoriteColor;
+      this.favoriteNumber = favoriteNumber;
+  }
+  
+  multiplyFavoriteNumber(times){ 
+  return this.favoriteNumber * times;
+  }
+}
+
+var person = new Person("Misael", "Calvillo", "purple", 50);
+var answer = person.multiplyFavoriteNumber(10); // 500
+
+console.log(answer);
